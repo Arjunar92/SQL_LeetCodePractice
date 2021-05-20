53516fa4ab8dcc2df169fdbff9ab63d0966a97f2
Arjun Achuthan <35266145+Arjunar92@users.noreply.github.com>
2021-05-20 18:48:04 -0400

    Create 569 Median_Employee_Salary.sql

---
+++ 569 Median_Employee_Salary.sql
@@ -0,0 +1,24 @@
+
+with temp as (
+select id, company , salary, 
+row_number() over(partition by company order by salary asc) as rk,
+count(id) over (partition by company) as ct
+from employee
+order by company , salary desc
+)
+
+
+
+select id, company , salary
+from temp
+where
+rk in ((case 
+               when MOD(ct,2)=1 then ((ct+1)/2) 
+               when MOD(ct,2)=0 then (ct/2)
+               else 0 
+               end),
+               (case 
+               when MOD(ct,2)=1 then ((ct+1)/2) 
+               when MOD(ct,2)=0 then (ct/2)+1
+               else 0 
+               end) )
