6183e517625663c72d078db7218a5b0008ce3bf9
Arjun Achuthan <35266145+Arjunar92@users.noreply.github.com>
2021-05-22 23:33:06 -0400

    Create 570. M - Managers with at Least 5 Direct Reports.sql

---
+++ 570. M - Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,14 @@
+#The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
+#Given the Employee table, write a SQL query that finds out managers with at least 5 direct report. For the above table, your SQL query should return:
+
+
+with temp as
+(
+select managerid, count(managerid) as manager_num from employee
+group by managerid
+having manager_num > 4
+)
+
+select name from employee e inner join temp t
+on e.id = t.managerid
