ff2a4816e9075ef8a3ddf13f1db224143b1b985f
Arjun Achuthan <35266145+Arjunar92@users.noreply.github.com>
2021-05-27 23:55:41 -0400

    Create 1045. M -  Customers Who Bought All Products.sql

---
+++ 1045. M -  Customers Who Bought All Products.sql
@@ -0,0 +1,18 @@
+#Write an SQL query for a report that provides the customer ids from the Customer table that bought all the products in the Product table.
+
+#Return the result table in any order.
+
+#The query result format is in the following example:
+
+ 
+
+
+# Write your MySQL query statement below
+
+select customer_id from 
+(
+select customer_id , count( distinct product_key) as prod_num
+from Customer
+group by customer_id
+having prod_num = (select count(distinct product_key) from product) 
+    )a
