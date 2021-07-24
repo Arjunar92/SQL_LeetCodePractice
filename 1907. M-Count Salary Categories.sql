51db6163517cae53aae3993af92c751584a62ca1
Arjun Achuthan <35266145+Arjunar92@users.noreply.github.com>
2021-07-23 23:21:50 -0400

    Update 1907. M-Count Salary Categories.sql

--- 1907. M-Count Salary Categories.sql
+++ 1907. M-Count Salary Categories.sql
@@ -4,7 +4,7 @@
 #"Low Salary": All the salaries strictly less than $20000.
 #"Average Salary": All the salaries in the inclusive range [$20000, $50000].
 #"High Salary": All the salaries strictly greater than $50000.
-#The result table must contain all three categories. If there are no accounts in a category, then report 0. Return the result table in any order.
+#The result table must contain all three categories. If there are no accounts in a category, then report 0.Return the result table in any order
 
 
 with cte1 as
